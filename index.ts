8c5e682075ba11e68086d064e80b84c5b2f98191
ermitarojo <tincarandang@hotmail.com>
2019-11-07 22:15:09 +1300

    1st commit yey

--- index.ts
+++ index.ts
@@ -4,3 +4,11 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+document.body.textContent = greeter(user);
